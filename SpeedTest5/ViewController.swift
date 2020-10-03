d2aea523e6f35f13ea6c447834cf2f5259dafbde
Farah-AlBader <70542682+Farah-AlBader@users.noreply.github.com>
2020-10-03 17:05:41 +0300

    done!

--- SpeedTest5/ViewController.swift
+++ SpeedTest5/ViewController.swift
@@ -1,26 +1,32 @@
+//
+//  ViewController.swift
+//  random 1
+//
+//  Created by Mac on 10/3/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
 import UIKit
 
 class ViewController: UIViewController {
     
-    @IBAction func red() {
-        view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-        
-    }
+    override func viewDidLoad() {
+           super.viewDidLoad()
+           // Do any additional setup after loading the view.
+       }
     
-    @IBAction func blue() {
+    @IBAction func red (_ sender: Any) {
+        view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+    }
+    @IBAction func blue (_ sender: Any) {
         view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
     }
-    
-    
-    
-    @IBAction func yellow() {
+    @IBAction func yellow (_ sender: Any) {
         view.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
     }
-    
-    @IBAction func green() {
+    @IBAction func green (_ sender: Any) {
         view.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
     }
-    
     
 }
 
